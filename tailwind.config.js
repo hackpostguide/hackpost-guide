715085ef7919f14351f9f25b247a792ebd053347
Alex <53095140+CometBlazer@users.noreply.github.com>
2023-11-22 19:11:42 -0500

    Update button styles and colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,29 +4,24 @@ module.exports = {
     './nuxt.config.ts',
     './content/**/*.md'
   ],
+  
+  darkMode: 'class',
+  // darkMode: ['class', '[data-theme="dark"]'],
   theme: {
     extend: {
-      // fontSize: {
-      //   'Heading1': '3rem',   // Customize the font size for Heading1
-      //   'Heading2': '2.5rem', // Customize the font size for Heading2
-      //   'md': '4rem',         // Define font size for medium screens (md)
-      //   'lg': '5rem',         // Define font size for large screens (lg)
-      // },
+      colors: {
+        'text': 'var(--text)',
+        'background': 'var(--background)',
+        'primary': 'var(--primary)',
+        'secondary': 'var(--secondary)',
+        'accent': 'var(--accent)',
+       },
     },
   },
-  colors: {
-    'text': '#c7c7c7',
-    'background': '#050510',
-    'primary': '#7d26c9',
-    'secondary': '#53570a',
-    'accent': '#ebb22d',
-   },
    
   plugins: [
     require('@tailwindcss/typography'),
     // ...
   ],
-  darkMode: 'class',
-  // darkMode: ['class', '[data-theme="dark"]'],
 }
 
