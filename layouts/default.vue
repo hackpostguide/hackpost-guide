c98622eb2e05799d0943069ef95be0ecf04fc971
Alex <53095140+CometBlazer@users.noreply.github.com>
2023-11-25 11:40:06 -0500

    Refactored default layout to use flexbox

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,9 +1,9 @@
 <template> <!--Default layout, will always apply unless another layout overrides it-->
-    <div class="">
+    <div class="flex flex-col h-screen">
       
       <Navbar />
 
-      <div class="p-10 mt-11" :style="{ background: 'var(--background)' }">
+      <div class="flex-grow p-10 mt-11" :style="{ background: 'var(--background)' }">
           <slot /> <!--This is where the page content goes-->
       </div>
 
@@ -13,13 +13,5 @@
   </template>
   
   <style scoped>
-    body {
-        min-height: 100vh; /* Make sure the body covers the entire viewport height */
-        display: flex;
-        flex-direction: column;
-    }
-    
-    .content {
-        flex-grow: 1; /* Allow content to expand and push the footer down */
-    }
+
 </style>
