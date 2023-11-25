2bc3c7f26909b6b441f64883e3fb93151985e0ab
Turtlez200 <88110937+adikeshn@users.noreply.github.com>
2023-11-24 23:24:11 -0500

    Update Link.vue

--- components/App/Link.vue
+++ components/App/Link.vue
@@ -1,64 +1,75 @@
 <script setup lang="ts">
-    import type { RouteLocationRaw } from 'vue-router';
-    import { hasProtocol } from 'ufo';
-    
-    type NuxtLinkProps = {
-        to?: RouteLocationRaw;
-        href?: RouteLocationRaw;
-        external?: boolean;
-        replace?: boolean;
-        target?: '_blank' | '_self' | '_parent' | '_top';
-        rel?: string;
-        prefetch?: boolean;
-    }
+import type { RouteLocationRaw } from "vue-router";
+import { hasProtocol } from "ufo";
 
-    const props = defineProps<NuxtLinkProps>();
-    const NuxtLink = defineNuxtLink({
-        trailingSlash: 'append',
-    });
+type NuxtLinkProps = {
+  to?: RouteLocationRaw;
+  href?: RouteLocationRaw;
+  external?: boolean;
+  replace?: boolean;
+  target?: "_blank" | "_self" | "_parent" | "_top";
+  rel?: string;
+  prefetch?: boolean;
+};
 
-    const path = computed(() => props.to || props.href || '');
+const props = defineProps<NuxtLinkProps>();
+const NuxtLink = defineNuxtLink({
+  trailingSlash: "append",
+});
+//random comment
 
-    //From 
-    const isExternal = computed(() => {
-        
-        //external prop is explicitly set:
-        if(props.external){
-            return true;
-        }
+const path = computed(() => props.to || props.href || "");
 
-        //when 'target' prop is set, link is external
-        if(props.target && props.target !== '_self'){
-            return true;
-        }
+//From
+const isExternal = computed(() => {
+  //external prop is explicitly set:
+  if (props.external) {
+    return true;
+  }
 
-        //when 'to' prop is a route object then it's an internal link
-        if(typeof path.value === 'object'){
-            return false;
-        }
+  //when 'target' prop is set, link is external
+  if (props.target && props.target !== "_self") {
+    return true;
+  }
 
-        return hasProtocol(path.value, { acceptRelative: true });
-    });
+  //when 'to' prop is a route object then it's an internal link
+  if (typeof path.value === "object") {
+    return false;
+  }
 
-    const propsWithoutRelAndTarget = computed(() => {
-        const { rel, target, ...rest } = props;
-        return rest;
-    })
+  return hasProtocol(path.value, { acceptRelative: true });
+});
 
-    // TODO: Add exceptions for certain domains if needed
-    const ALLOWED_DOMAINS = [
-        "imaginai.dev",
-    ]
-    const isDomainException = computed(() => typeof path.value === 'string' && ALLOWED_DOMAINS.some(domain => (path.value as string).startsWith(domain)))
-    const defaultRel = computed(() => isExternal.value && !isDomainException.value ? 'nofollow noindex noreferrer' : null)
-    const relValue = computed(() => props.rel ?? defaultRel.value)
+const propsWithoutRelAndTarget = computed(() => {
+  const { rel, target, ...rest } = props;
+  return rest;
+});
 
-    const defaultTarget = computed(() => isExternal.value ? '_blank' : '')
-    const targetValue = computed(() => props.target ?? defaultTarget.value)
-    </script>
+// TODO: Add exceptions for certain domains if needed
+const ALLOWED_DOMAINS = ["imaginai.dev"];
+const isDomainException = computed(
+  () =>
+    typeof path.value === "string" &&
+    ALLOWED_DOMAINS.some((domain) => (path.value as string).startsWith(domain))
+);
+const defaultRel = computed(() =>
+  isExternal.value && !isDomainException.value
+    ? "nofollow noindex noreferrer"
+    : null
+);
+const relValue = computed(() => props.rel ?? defaultRel.value);
+
+const defaultTarget = computed(() => (isExternal.value ? "_blank" : ""));
+const targetValue = computed(() => props.target ?? defaultTarget.value);
+</script>
 
 <template>
-  <component :is="NuxtLink" v-bind="propsWithoutRelAndTarget" :rel="relValue" :target="targetValue">
+  <component
+    :is="NuxtLink"
+    v-bind="propsWithoutRelAndTarget"
+    :rel="relValue"
+    :target="targetValue"
+  >
     <slot />
   </component>
 </template>
