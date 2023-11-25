030fa6f23e91e4b7a948d8827e025d5ccc164fc2
Alex <53095140+CometBlazer@users.noreply.github.com>
2023-11-24 23:40:29 -0500

    Applied CSS grid layout to courses page

--- pages/courses/index.vue
+++ pages/courses/index.vue
@@ -1,13 +1,14 @@
 <template>
     <!-- Todo: apply css grids for entire page, fix paddings/margins -->
-    <div class="m-auto">
+    <div class="m-auto grid grid-cols-12">
 
-        <div class="py-10">
+        <div class="py-10 col-start-2 col-end-12">
             <p class="Heading2 font-bold mb-4">Header Text</p>
             <p class="bodyText">Lorem ipsum dolor sit amet consectetur adipisicing elit. Odit ea dolorum a saepe porro consectetur sit! Laudantium maxime ullam molestias.</p>
         </div>
 
-        <div class="grid grid-cols-1 sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8 mt-4">
+        <div class="col-start-2 col-end-12 grid grid-cols-1 sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8 mt-4">
+        <!-- <div class="grid grid-cols-1 sm:grid-cols-1 md:grid-cols-2 lg:grid-cols    -3 gap-8 mt-4"> -->
             <div class="col-span-1 ">
                 <ModuleCard to="/courses/test" />
             </div> 
