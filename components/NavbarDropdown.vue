51e233d96e0409d6ec80a50ccf217f4ee17f6f80
Alex <53095140+CometBlazer@users.noreply.github.com>
2023-11-27 22:08:20 -0500

    fixed navbar height changing bug

--- components/NavbarDropdown.vue
+++ components/NavbarDropdown.vue
@@ -4,32 +4,34 @@
             <li> 
                 <Icon name="uil:bars" class="h-[24px] w-[24px]" @click="toggle"/>
             </li>
-            <li v-if="active"><NuxtLink to="/courses">Modules</NuxtLink></li>
-            <li v-if="active"><NuxtLink to="/about">About</NuxtLink></li>
-            <!-- Will implement firebase sign-in later -->
-            <li v-if="active"><NuxtLink to="/signup">Sign Up</NuxtLink></li>
+            <li v-if="active" :class="{'active-link': active}"><NuxtLink to="/courses">Modules</NuxtLink></li>
+            <li v-if="active" :class="{'active-link': active}"><NuxtLink to="/about">About</NuxtLink></li>
+            <li v-if="active" :class="{'active-link': active}"><NuxtLink to="/signup">Sign Up</NuxtLink></li>
         </div>
     </ul>
 </template>
 
 <script>
-  export default {
-    data () {
-      return {
-        active: false
-      }
-    },
-    methods: {
-      toggle () {
-        this.active = !this.active
-      }
+export default {
+  data () {
+    return {
+      active: false
+    }
+  },
+  methods: {
+    toggle () {
+      this.active = !this.active
     }
   }
+}
 </script>
 
 
 <style lang="scss" scoped>
-    .dropdown li {
-        padding: 5px;
-    }
+
+.active-link {
+    // Apply padding and text alignment when active
+    padding: 5px;
+    text-align: left;
+}
 </style>
