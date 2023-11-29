6472d2e5af01b6e096a9c14f9365b7794760a33b
Alex <53095140+CometBlazer@users.noreply.github.com>
2023-11-28 22:03:11 -0500

    Created transparent button style

--- components/App/Button.vue
+++ components/App/Button.vue
@@ -1,24 +1,27 @@
 <template>
-<router-link :to="props.to" :class="buttonClass">
-    <slot />
-</router-link>
-</template>
-
-<script setup lang="ts">
-import { computed, defineProps, withDefaults } from 'vue';
-import type { RouteRecordRaw } from 'vue-router';
-
-// Define button styles
-const props = withDefaults(defineProps<{
-    buttonStyle?: 'major' | 'minor';
+    <router-link :to="props.to" :class="buttonClass">
+      <slot />
+    </router-link>
+  </template>
+  
+  <script setup lang="ts">
+  import { computed, defineProps, withDefaults } from 'vue';
+  import type { RouteRecordRaw } from 'vue-router';
+  
+  // Define button styles
+  const props = withDefaults(defineProps<{
+    buttonStyle?: 'major' | 'minor' | 'transparent';
     to?: string | RouteRecordRaw['path'];
-}>(), {
+  }>(), {
     buttonStyle: 'major',
     to: '/',
-});
+  });
+  
+  // Compute the class based on the button style prop
+  const buttonClass = computed(() => {
+    return props.buttonStyle === 'major' ? 'btn' : props.buttonStyle === 'minor' ? 'btn-minor' : 'btn-transparent';
+  });
 
-// Compute the class based on the button style prop
-const buttonClass = computed(() => {
-return props.buttonStyle === 'major' ? 'btn' : 'btn-minor';
-});
-</script>
+
+  //Syntax: <AppButton buttonStyle="major" to="/courses">Get Started</AppButton>
+  </script>
