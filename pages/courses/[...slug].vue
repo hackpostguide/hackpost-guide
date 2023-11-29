dc33bbca2b262c4fc69b769c75b36750d50c28a3
Alex <53095140+CometBlazer@users.noreply.github.com>
2023-11-28 21:53:27 -0500

    Refactored blog post layout and add social media sharing buttons

--- pages/courses/[...slug].vue
+++ pages/courses/[...slug].vue
@@ -1,15 +1,49 @@
 <template>
-    <div class="my-8 flex flex-col items-center">
-      <div class="text-center"> <!-- Center the text within this container -->
-        <p class="Heading2 my-4">Blog Post</p>
-        <p class="bodyText">By me</p>
-      </div>
-  
-      <div class="my-10 flex flex-col items-start"> <!-- Align items to the start (left) -->
-        <AppButton buttonStyle="minor" to="/courses" class="my-6">Back</AppButton>
-        <main class="prose dark:prose-invert">
-          <ContentDoc />
-        </main>
+    <div class="grid grid-cols-12">
+    <section class="my-3 col-start-2 col-end-5">
+      <AppButton buttonStyle="minor" to="/courses">
+          <Icon name="uil:angle-left-b" class="h-[24px] w-[24px]"/>
+          Back
+      </AppButton>
+    </section>
+      <div class="my-8 flex flex-col items-center col-start-1 col-end-12">
+        <div class="text-center"> 
+          <p class="Heading2 my-4">Blog Post</p>
+          <p class="bodyText">By me</p>
+        </div>
+    
+        <div class="my-10 flex flex-col items-start"> 
+
+          <main class="prose dark:prose-invert">
+            <ContentDoc />
+          </main>
+        </div>
+
+        <div class="flex flex-col items-center">
+          <p class="bodyText">Share this post!</p>
+          <div class="flex gap-4">
+            <AppButton buttonStyle="minor" to="/">
+              <Icon name="uil:twitter" class="h-[24px] w-[24px]"/>
+            </AppButton>
+            <AppButton buttonStyle="minor" to="/">
+              <Icon name="uil:facebook" class="h-[24px] w-[24px]"/>
+            </AppButton>
+            <AppButton buttonStyle="minor" to="/">
+              <Icon name="uil:instagram" class="h-[24px] w-[24px]"/>
+            </AppButton>
+          </div>
+        </div>
+
+        <div class="flex gap-4 my-4">
+          <AppButton buttonStyle="minor" to="/courses">
+            <Icon name="uil:angle-left-b" class="h-[24px] w-[24px]"/>
+            Previous Post
+          </AppButton>
+          <AppButton buttonStyle="minor" to="/courses">
+            Next Post
+            <Icon name="uil:angle-right-b" class="h-[24px] w-[24px]"/>
+          </AppButton>
+        </div>
       </div>
     </div>
   </template>
