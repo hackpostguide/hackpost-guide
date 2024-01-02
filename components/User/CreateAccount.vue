c217a50462d6462c5a7cad654cb6746a4bdcff3e
Alex <53095140+CometBlazer@users.noreply.github.com>
2024-01-01 20:03:56 -0500

    created a userCreateAccount component using vueform

---
+++ components/User/CreateAccount.vue
@@ -0,0 +1,490 @@
+<script>
+import { ref } from 'vue'
+import { useVueform, Vueform } from '@vueform/vueform'
+
+export default {
+  mixins: [Vueform],
+  setup(props, context)
+  {
+    const form = useVueform(props, context)
+
+    const vueform = ref({
+      size: 'md',
+      displayErrors: false,
+      steps: {
+        page0: {
+          label: 'Step 1',
+          elements: [
+            'page_title',
+            'divider',
+            'container',
+            'username',
+            'email',
+            'password',
+            'password_confirmation',
+            'terms',
+            'marketing_emails',
+            'divider_1',
+            'register',
+          ],
+        },
+        page1: {
+          label: 'Step 2',
+          elements: [
+            'h1',
+            'p',
+            'primaryButton',
+            'p_copy',
+            'secondaryButton',
+          ],
+        },
+        page2: {
+          label: 'Step 3',
+          elements: [
+            'h1_1',
+            'textarea',
+          ],
+        },
+      },
+      addClass: 'vf-create-account',
+      schema: {
+        page_title: {
+          type: 'static',
+          content: 'Create account',
+          tag: 'h1',
+        },
+        divider: {
+          type: 'static',
+          tag: 'hr',
+        },
+        container: {
+          type: 'group',
+          schema: {
+            first_name: {
+              type: 'text',
+              placeholder: 'First name',
+              columns: {
+                container: 6,
+                label: 12,
+                wrapper: 12,
+              },
+              fieldName: 'First name',
+              rules: [
+                'required',
+                'max:255',
+              ],
+            },
+            last_name: {
+              type: 'text',
+              placeholder: 'Last name',
+              columns: {
+                container: 6,
+                label: 12,
+                wrapper: 12,
+              },
+              fieldName: 'Last name',
+              rules: [
+                'required',
+                'max:255',
+              ],
+            },
+          },
+        },
+        username: {
+          type: 'text',
+          placeholder: 'Username',
+          rules: [
+            'required',
+          ],
+          fieldName: 'Phone',
+          description: "You won't be able to change your username after you create your account",
+        },
+        email: {
+          type: 'text',
+          inputType: 'email',
+          rules: [
+            'required',
+            'max:255',
+            'email',
+          ],
+          placeholder: 'Email',
+          fieldName: 'Email',
+          description: 'You will receive a confirmation letter to this email.',
+        },
+        password: {
+          type: 'text',
+          inputType: 'password',
+          rules: [
+            'required',
+            'min:8',
+            'same:password_confirmation',
+          ],
+          fieldName: 'Password',
+          placeholder: 'Password',
+        },
+        password_confirmation: {
+          type: 'text',
+          inputType: 'password',
+          rules: [
+            'required',
+          ],
+          fieldName: 'Password confirmation',
+          placeholder: 'Password again',
+        },
+        terms: {
+          type: 'checkbox',
+          text: 'I accept the Terms & Conditions & Privacy Policy',
+        },
+        marketing_emails: {
+          type: 'checkbox',
+          text: 'Add me to the mailing list',
+        },
+        divider_1: {
+          type: 'static',
+          tag: 'hr',
+        },
+        register: {
+          type: 'button',
+          submits: true,
+          buttonLabel: 'Create account',
+          full: true,
+          size: 'lg',
+        },
+        h1: {
+          type: 'static',
+          tag: 'h1',
+          content: 'Check to confirm your email',
+        },
+        p: {
+          type: 'static',
+          tag: 'p',
+          content: 'After you confirmed your email, click "I confirmed my email" to continue!',
+        },
+        primaryButton: {
+          type: 'button',
+          buttonLabel: 'I confirmed my email!',
+          submits: true,
+          align: 'center',
+          full: true,
+        },
+        p_copy: {
+          type: 'static',
+          tag: 'p',
+          content: 'If you didn\'t receive an email, click "resend" to resend the confirmation email.',
+        },
+        secondaryButton: {
+          type: 'button',
+          buttonLabel: 'Resend confirmation email',
+          secondary: true,
+          submits: true,
+          full: true,
+        },
+        h1_1: {
+          type: 'static',
+          tag: 'h1',
+          content: 'Onboarding',
+        },
+        textarea: {
+          type: 'textarea',
+          label: 'Optional: add your own bio!',
+          default: '150 characters max',
+        },
+      },
+    })
+
+    return {
+      ...form,
+      vueform,
+    }
+  }
+}
+</script>
+
+<style>
+.vf-create-account *,
+.vf-create-account *:before,
+.vf-create-account *:after,
+.vf-create-account:root {
+  --vf-primary: #07bf9b;
+  --vf-primary-darker: #06ac8b;
+  --vf-color-on-primary: #ffffff;
+  --vf-danger: #ef4444;
+  --vf-danger-lighter: #fee2e2;
+  --vf-success: #10b981;
+  --vf-success-lighter: #d1fae5;
+  --vf-gray-50: #f9fafb;
+  --vf-gray-100: #f3f4f6;
+  --vf-gray-200: #e5e7eb;
+  --vf-gray-300: #d1d5db;
+  --vf-gray-400: #9ca3af;
+  --vf-gray-500: #6b7280;
+  --vf-gray-600: #4b5563;
+  --vf-gray-700: #374151;
+  --vf-gray-800: #1f2937;
+  --vf-gray-900: #111827;
+  --vf-ring-width: 2px;
+  --vf-ring-color: #07bf9b66;
+  --vf-link-color: var(--vf-primary);
+  --vf-link-decoration: inherit;
+  --vf-font-size: 1rem;
+  --vf-font-size-sm: 0.875rem;
+  --vf-font-size-lg: 1rem;
+  --vf-font-size-small: 0.875rem;
+  --vf-font-size-small-sm: 0.8125rem;
+  --vf-font-size-small-lg: 0.875rem;
+  --vf-font-size-h1: 2.125rem;
+  --vf-font-size-h1-sm: 2.125rem;
+  --vf-font-size-h1-lg: 2.125rem;
+  --vf-font-size-h2: 1.875rem;
+  --vf-font-size-h2-sm: 1.875rem;
+  --vf-font-size-h2-lg: 1.875rem;
+  --vf-font-size-h3: 1.5rem;
+  --vf-font-size-h3-sm: 1.5rem;
+  --vf-font-size-h3-lg: 1.5rem;
+  --vf-font-size-h4: 1.25rem;
+  --vf-font-size-h4-sm: 1.25rem;
+  --vf-font-size-h4-lg: 1.25rem;
+  --vf-font-size-h1-mobile: 1.5rem;
+  --vf-font-size-h1-mobile-sm: 1.5rem;
+  --vf-font-size-h1-mobile-lg: 1.5rem;
+  --vf-font-size-h2-mobile: 1.25rem;
+  --vf-font-size-h2-mobile-sm: 1.25rem;
+  --vf-font-size-h2-mobile-lg: 1.25rem;
+  --vf-font-size-h3-mobile: 1.125rem;
+  --vf-font-size-h3-mobile-sm: 1.125rem;
+  --vf-font-size-h3-mobile-lg: 1.125rem;
+  --vf-font-size-h4-mobile: 1rem;
+  --vf-font-size-h4-mobile-sm: 1rem;
+  --vf-font-size-h4-mobile-lg: 1rem;
+  --vf-font-size-blockquote: 1rem;
+  --vf-font-size-blockquote-sm: 0.875rem;
+  --vf-font-size-blockquote-lg: 1rem;
+  --vf-line-height: 1.5rem;
+  --vf-line-height-sm: 1.25rem;
+  --vf-line-height-lg: 1.5rem;
+  --vf-line-height-small: 1.25rem;
+  --vf-line-height-small-sm: 1.125rem;
+  --vf-line-height-small-lg: 1.25rem;
+  --vf-line-height-headings: 1.2;
+  --vf-line-height-headings-sm: 1.2;
+  --vf-line-height-headings-lg: 1.2;
+  --vf-line-height-blockquote: 1.5rem;
+  --vf-line-height-blockquote-sm: 1.25rem;
+  --vf-line-height-blockquote-lg: 1.5rem;
+  --vf-letter-spacing: 0px;
+  --vf-letter-spacing-sm: 0px;
+  --vf-letter-spacing-lg: 0px;
+  --vf-letter-spacing-small: 0px;
+  --vf-letter-spacing-small-sm: 0px;
+  --vf-letter-spacing-small-lg: 0px;
+  --vf-letter-spacing-headings: 0px;
+  --vf-letter-spacing-headings-sm: 0px;
+  --vf-letter-spacing-headings-lg: 0px;
+  --vf-letter-spacing-blockquote: 0px;
+  --vf-letter-spacing-blockquote-sm: 0px;
+  --vf-letter-spacing-blockquote-lg: 0px;
+  --vf-gutter: 1rem;
+  --vf-gutter-sm: 0.5rem;
+  --vf-gutter-lg: 1rem;
+  --vf-min-height-input: 2.375rem;
+  --vf-min-height-input-sm: 2.125rem;
+  --vf-min-height-input-lg: 2.875rem;
+  --vf-py-input: 0.375rem;
+  --vf-py-input-sm: 0.375rem;
+  --vf-py-input-lg: 0.625rem;
+  --vf-px-input: 0.75rem;
+  --vf-px-input-sm: 0.5rem;
+  --vf-px-input-lg: 0.875rem;
+  --vf-py-btn: 0.375rem;
+  --vf-py-btn-sm: 0.375rem;
+  --vf-py-btn-lg: 0.625rem;
+  --vf-px-btn: 0.875rem;
+  --vf-px-btn-sm: 0.75rem;
+  --vf-px-btn-lg: 1.25rem;
+  --vf-py-btn-small: 0.25rem;
+  --vf-py-btn-small-sm: 0.25rem;
+  --vf-py-btn-small-lg: 0.375rem;
+  --vf-px-btn-small: 0.625rem;
+  --vf-px-btn-small-sm: 0.625rem;
+  --vf-px-btn-small-lg: 0.75rem;
+  --vf-py-group-tabs: 0.375rem;
+  --vf-py-group-tabs-sm: 0.375rem;
+  --vf-py-group-tabs-lg: 0.625rem;
+  --vf-px-group-tabs: 0.75rem;
+  --vf-px-group-tabs-sm: 0.5rem;
+  --vf-px-group-tabs-lg: 0.875rem;
+  --vf-py-group-blocks: 0.75rem;
+  --vf-py-group-blocks-sm: 0.625rem;
+  --vf-py-group-blocks-lg: 0.875rem;
+  --vf-px-group-blocks: 1rem;
+  --vf-px-group-blocks-sm: 1rem;
+  --vf-px-group-blocks-lg: 1rem;
+  --vf-py-tag: 0px;
+  --vf-py-tag-sm: 0px;
+  --vf-py-tag-lg: 0px;
+  --vf-px-tag: 0.4375rem;
+  --vf-px-tag-sm: 0.4375rem;
+  --vf-px-tag-lg: 0.4375rem;
+  --vf-py-slider-tooltip: 0.125rem;
+  --vf-py-slider-tooltip-sm: 0.0625rem;
+  --vf-py-slider-tooltip-lg: 0.1875rem;
+  --vf-px-slider-tooltip: 0.375rem;
+  --vf-px-slider-tooltip-sm: 0.3125rem;
+  --vf-px-slider-tooltip-lg: 0.5rem;
+  --vf-py-blockquote: 0.25rem;
+  --vf-py-blockquote-sm: 0.25rem;
+  --vf-py-blockquote-lg: 0.25rem;
+  --vf-px-blockquote: 0.75rem;
+  --vf-px-blockquote-sm: 0.75rem;
+  --vf-px-blockquote-lg: 0.75rem;
+  --vf-py-hr: 0.25rem;
+  --vf-space-addon: 0px;
+  --vf-space-addon-sm: 0px;
+  --vf-space-addon-lg: 0px;
+  --vf-space-checkbox: 0.375rem;
+  --vf-space-checkbox-sm: 0.375rem;
+  --vf-space-checkbox-lg: 0.375rem;
+  --vf-space-tags: 0.1875rem;
+  --vf-space-tags-sm: 0.1875rem;
+  --vf-space-tags-lg: 0.1875rem;
+  --vf-space-static-tag-1: 1rem;
+  --vf-space-static-tag-2: 2rem;
+  --vf-space-static-tag-3: 3rem;
+  --vf-floating-top: 0rem;
+  --vf-floating-top-sm: 0rem;
+  --vf-floating-top-lg: 0.6875rem;
+  --vf-bg-input: #ffffff;
+  --vf-bg-input-hover: #ffffff;
+  --vf-bg-input-focus: #ffffff;
+  --vf-bg-input-danger: #ffffff;
+  --vf-bg-input-success: #ffffff;
+  --vf-bg-disabled: var(--vf-gray-200);
+  --vf-bg-selected: #1118270d;
+  --vf-bg-passive: var(--vf-gray-300);
+  --vf-bg-icon: var(--vf-gray-500);
+  --vf-bg-danger: var(--vf-danger-lighter);
+  --vf-bg-success: var(--vf-success-lighter);
+  --vf-bg-tag: var(--vf-primary);
+  --vf-bg-slider-handle: var(--vf-primary);
+  --vf-bg-toggle-handle: #ffffff;
+  --vf-bg-date-head: var(--vf-gray-100);
+  --vf-bg-addon: #ffffff00;
+  --vf-bg-btn: var(--vf-primary);
+  --vf-bg-btn-danger: var(--vf-danger);
+  --vf-bg-btn-secondary: var(--vf-gray-200);
+  --vf-color-input: var(--vf-gray-800);
+  --vf-color-input-hover: var(--vf-gray-800);
+  --vf-color-input-focus: var(--vf-gray-800);
+  --vf-color-input-danger: var(--vf-gray-800);
+  --vf-color-input-success: var(--vf-gray-800);
+  --vf-color-disabled: var(--vf-gray-400);
+  --vf-color-placeholder: var(--vf-gray-300);
+  --vf-color-passive: var(--vf-gray-700);
+  --vf-color-muted: var(--vf-gray-500);
+  --vf-color-floating: var(--vf-gray-500);
+  --vf-color-floating-focus: var(--vf-gray-500);
+  --vf-color-floating-success: var(--vf-gray-500);
+  --vf-color-floating-danger: var(--vf-gray-500);
+  --vf-color-danger: var(--vf-danger);
+  --vf-color-success: var(--vf-success);
+  --vf-color-tag: var(--vf-color-on-primary);
+  --vf-color-addon: var(--vf-gray-800);
+  --vf-color-date-head: var(--vf-gray-700);
+  --vf-color-btn: var(--vf-color-on-primary);
+  --vf-color-btn-danger: #ffffff;
+  --vf-color-btn-secondary: var(--vf-gray-700);
+  --vf-border-color-input: var(--vf-gray-300);
+  --vf-border-color-input-hover: var(--vf-gray-300);
+  --vf-border-color-input-focus: var(--vf-primary);
+  --vf-border-color-input-danger: var(--vf-gray-300);
+  --vf-border-color-input-success: var(--vf-gray-300);
+  --vf-border-color-checked: var(--vf-primary);
+  --vf-border-color-passive: var(--vf-gray-300);
+  --vf-border-color-slider-tooltip: var(--vf-primary);
+  --vf-border-color-tag: var(--vf-primary);
+  --vf-border-color-btn: var(--vf-primary);
+  --vf-border-color-btn-danger: var(--vf-danger);
+  --vf-border-color-btn-secondary: var(--vf-gray-200);
+  --vf-border-color-blockquote: var(--vf-gray-300);
+  --vf-border-color-hr: var(--vf-gray-300);
+  --vf-border-width-input-t: 1px;
+  --vf-border-width-input-r: 1px;
+  --vf-border-width-input-b: 1px;
+  --vf-border-width-input-l: 1px;
+  --vf-border-width-radio-t: 1px;
+  --vf-border-width-radio-r: 1px;
+  --vf-border-width-radio-b: 1px;
+  --vf-border-width-radio-l: 1px;
+  --vf-border-width-checkbox-t: 1px;
+  --vf-border-width-checkbox-r: 1px;
+  --vf-border-width-checkbox-b: 1px;
+  --vf-border-width-checkbox-l: 1px;
+  --vf-border-width-dropdown: 1px;
+  --vf-border-width-btn: 1px;
+  --vf-border-width-toggle: 0.125rem;
+  --vf-border-width-tag: 1px;
+  --vf-border-width-blockquote: 3px;
+  --vf-shadow-input: 0px 0px 0px 0px rgba(0,0,0,0);
+  --vf-shadow-input-hover: 0px 0px 0px 0px rgba(0,0,0,0);
+  --vf-shadow-input-focus: 0px 0px 0px 0px rgba(0,0,0,0);
+  --vf-shadow-handles: 0px 0px 0px 0px rgba(0,0,0,0);
+  --vf-shadow-handles-hover: 0px 0px 0px 0px rgba(0,0,0,0);
+  --vf-shadow-handles-focus: 0px 0px 0px 0px rgba(0,0,0,0);
+  --vf-shadow-btn: 0px 0px 0px 0px rgba(0,0,0,0);
+  --vf-shadow-dropdown: 0px 0px 0px 0px rgba(0,0,0,0);
+  --vf-radius-input: 0.25rem;
+  --vf-radius-input-sm: 0.25rem;
+  --vf-radius-input-lg: 0.25rem;
+  --vf-radius-btn: 0.25rem;
+  --vf-radius-btn-sm: 0.25rem;
+  --vf-radius-btn-lg: 0.25rem;
+  --vf-radius-small: 0.25rem;
+  --vf-radius-small-sm: 0.25rem;
+  --vf-radius-small-lg: 0.25rem;
+  --vf-radius-large: 0.25rem;
+  --vf-radius-large-sm: 0.25rem;
+  --vf-radius-large-lg: 0.25rem;
+  --vf-radius-tag: 0.25rem;
+  --vf-radius-tag-sm: 0.25rem;
+  --vf-radius-tag-lg: 0.25rem;
+  --vf-radius-checkbox: 0.25rem;
+  --vf-radius-checkbox-sm: 0.25rem;
+  --vf-radius-checkbox-lg: 0.25rem;
+  --vf-radius-slider: 0.25rem;
+  --vf-radius-slider-sm: 0.25rem;
+  --vf-radius-slider-lg: 0.25rem;
+  --vf-radius-image: 0.25rem;
+  --vf-radius-image-sm: 0.25rem;
+  --vf-radius-image-lg: 0.25rem;
+  --vf-radius-gallery: 0.25rem;
+  --vf-radius-gallery-sm: 0.25rem;
+  --vf-radius-gallery-lg: 0.25rem;
+  --vf-checkbox-size: 1rem;
+  --vf-checkbox-size-sm: 0.875rem;
+  --vf-checkbox-size-lg: 1rem;
+  --vf-gallery-size: 6rem;
+  --vf-gallery-size-sm: 5rem;
+  --vf-gallery-size-lg: 7rem;
+  --vf-toggle-width: 3rem;
+  --vf-toggle-width-sm: 2.75rem;
+  --vf-toggle-width-lg: 3rem;
+  --vf-toggle-height: 1.25rem;
+  --vf-toggle-height-sm: 1rem;
+  --vf-toggle-height-lg: 1.25rem;
+  --vf-slider-height: 0.375rem;
+  --vf-slider-height-sm: 0.3125rem;
+  --vf-slider-height-lg: 0.5rem;
+  --vf-slider-height-vertical: 20rem;
+  --vf-slider-height-vertical-sm: 20rem;
+  --vf-slider-height-vertical-lg: 20rem;
+  --vf-slider-handle-size: 1rem;
+  --vf-slider-handle-size-sm: 0.875rem;
+  --vf-slider-handle-size-lg: 1.25rem;
+  --vf-slider-tooltip-distance: 0.5rem;
+  --vf-slider-tooltip-distance-sm: 0.375rem;
+  --vf-slider-tooltip-distance-lg: 0.5rem;
+  --vf-slider-tooltip-arrow-size: 0.3125rem;
+  --vf-slider-tooltip-arrow-size-sm: 0.3125rem;
+  --vf-slider-tooltip-arrow-size-lg: 0.3125rem;
+}
+</style>
---
+++ pages/signup1.vue
@@ -0,0 +1,230 @@
+<script lang="ts" setup>
+import {
+  createUserWithEmailAndPassword,
+  EmailAuthProvider,
+  linkWithCredential,
+  signInAnonymously,
+  signInWithEmailAndPassword,
+  signInWithPopup,
+  signInWithRedirect,
+  signOut,
+  GoogleAuthProvider,
+  updateCurrentUser,
+  updateProfile,
+  sendEmailVerification,
+  AuthCredential,
+  getRedirectResult,
+} from 'firebase/auth'
+
+import { ref, watch } from 'vue';
+import {
+  useCurrentUser,
+  useFirebaseAuth,
+  updateCurrentUserProfile,
+} from 'vuefire';
+// import { useCollection, useDocument } from 'vuefire';
+import { collection, doc, setDoc, getDoc, writeBatch } from 'firebase/firestore';
+
+const db = useFirestore();
+const auth = useFirebaseAuth()!;
+const user = useCurrentUser();
+let credential: AuthCredential | null = null;
+
+// const route = useRoute();
+const router = useRouter();
+
+// Redirect if already logged in
+// watch(user, (user) => {
+//   if (
+//     user &&
+//     route.query.redirect &&
+//     typeof route.query.redirect === 'string'
+//   ) {
+//     router.push(route.query.redirect);
+//   }
+// });
+
+
+//for some reason signing up and signing in are taking up 50 reads each - fix! 
+
+// New user refs
+const firstName = ref('');
+const lastName = ref('');
+const username = ref('');
+const email = ref('');
+const password = ref('');
+const errorMessage = ref(''); // Holds error messages for user feedback
+
+// const usersRef = useCollection(collection(db, 'users'));
+// const usernameRef = useCollection(collection(db, 'usernames'));
+
+async function signUp() {
+  try {
+    // Validate username and password constraints
+    if (!username.value) {
+      errorMessage.value = "Username cannot be empty";
+      return "Username cannot be empty";
+    } else if (username.value.length < 3) {
+      errorMessage.value = "Username must be at least 3 characters";
+      return "Username must be at least 3 characters";
+    } else if (username.value.length > 15) {
+      errorMessage.value = "Username must be less than 15 characters";
+      return "Username must be less than 20 characters";
+    } else if (!/^[a-zA-Z0-9_]+$/.test(username.value)) {
+      errorMessage.value = "Username can only contain letters, numbers, and underscores";
+      return "Username can only contain letters, numbers, and underscores";
+    } else if (password.value.length < 6) {
+      errorMessage.value = "Password must be at least 6 characters";
+      return "Password must be at least 6 characters";
+    }
+
+    // Check if the username is already taken
+    const usernameDocRef = doc(db, 'usernames', username.value);
+    const docSnapshot = await getDoc(usernameDocRef);
+    if (docSnapshot.exists()) {
+      errorMessage.value = "Username is already taken";
+      return "Username is already taken";
+    }
+
+    // Create user with email and password
+    const userCredential = await createUserWithEmailAndPassword(auth, email.value, password.value);
+    //TODO: save user info first, then send verification email
+
+    // Send verification email
+    await sendEmailVerification(userCredential.user);
+    console.log("Verification email sent.");
+
+    return "User registered successfully, verification email sent.";
+  } catch (error: any) {
+    errorMessage.value = error.message;
+    console.error("Signup failed:", error);
+    return "Registration failed: " + error.message;
+  }
+}
+
+async function signIn(){
+  if (auth.currentUser) {
+    await checkEmailVerification(auth.currentUser);
+  }
+  else{
+    signInWithEmailAndPassword(auth, email.value, password.value);
+    router.push('/'); // Redirect the user to a confirmed page
+  }
+}
+
+async function checkEmailVerification(user: { reload?: any; emailVerified?: any; uid: any; email?: any; }) {
+  await user.reload(); // Refresh the user's auth object
+  if (user.emailVerified) {
+    // Email is verified, proceed to save the user information
+    let displayName = firstName.value + " " + lastName.value;
+    try {
+      await saveUserInfo(user, displayName, username.value);
+      // Sign out after successfully saving user info
+      await auth.signOut();
+    } catch (error: any) {
+      console.error("Error saving user information:", error);
+      errorMessage.value = "Failed to save user information: " + error.message;
+    }
+  } else {
+    // Email not yet verified, you might want to check again or prompt the user
+    errorMessage.value = "Please verify your email to complete registration.";
+  }
+}
+
+function saveUserInfo(user: { reload?: any; emailVerified?: any; uid: any; email?: any; }, displayName: any, username: string) {
+  // Batch write to save user info and username
+  const batch = writeBatch(db);
+  const userRef = doc(db, 'users', user.uid);
+  batch.set(userRef, {
+    displayName: displayName,
+    username: username,
+    email: user.email,
+  }); 
+
+  const usernameRef = doc(db, 'usernames', username);
+  batch.set(usernameRef, { uid: user.uid });
+
+  batch.commit().then(() => {
+    console.log("User information saved!");
+  }).catch((error) => {
+    console.error("Error saving user information:", error);
+    errorMessage.value = "Failed to save user information: " + error.message;
+  });
+}
+
+// async function changeUserImage() {
+//   if (user.value) {
+//     await updateCurrentUserProfile({
+//       photoURL: 'https://i.pravatar.cc/150?u=' + Date.now(),
+//     });
+//   }
+// }
+
+</script>
+
+<template>
+  <div class="flex flex-col items-center justify-center min-h-screen">
+    <h1 class="Heading2">Sign Up</h1>
+
+    <form @submit.prevent="signUp()">
+      <fieldset>
+        <div class="py-5">
+          <label for="firstName" class="bodyText">First Name: </label>
+          <input v-model="firstName" type="text" required />
+        </div>
+        <div class="py-5">
+          <label for="lastName" class="bodyText">Last Name: </label>
+          <input v-model="lastName" type="text" required />
+        </div>
+        <div class="py-5">
+          <label for="username" class="bodyText">Username: </label>
+          <input v-model="username" type="text" required />
+        </div>
+        <div class="py-5">
+          <label for="email" class="bodyText">Email: </label>
+          <input v-model="email" type="email" required />
+        </div> 
+        <div class="py-5">
+          <label for="password" class="bodyText">Password: </label>
+          <input v-model="password" type="password" required />
+        </div> 
+
+        <button class="btn">Create User</button>
+      </fieldset>
+    </form>
+    <p>{{ errorMessage }}</p>
+
+
+    <h1 class="Heading2">Sign In</h1>
+    <form @submit.prevent="signIn()">
+      <fieldset>
+
+        <div class="py-5">
+          <label for="email" class="bodyText">Email: </label>
+          <input v-model="email" type="email" required />
+        </div> 
+        <div class="py-5">
+          <label for="password" class="bodyText">Password: </label>
+          <input v-model="password" type="password" required />
+        </div> 
+
+        <button class="btn">Sign In</button>
+      </fieldset>
+    </form>
+
+
+    <button class="btn" @click="signOut(auth)">Sign Out</button>
+
+    <!-- for debugging purposes only -->
+    <ClientOnly>
+      <p>Current User:</p>
+      <pre>{{ user }}</pre>
+    </ClientOnly>
+
+    <div class="mt-8 text-center">
+      <h2 class="Heading3">Or log instead:</h2>
+        <AppButton buttonStyle="minor" to="/login" class="m-5">Login</AppButton>
+    </div>
+  </div>
+</template>
+
