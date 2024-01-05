ae01539d03b36952f9d878764dae285a3bc56b90
Alex <53095140+CometBlazer@users.noreply.github.com>
2024-01-05 12:14:04 -0500

    modified user.ts pinia store to use options store

--- stores/user.ts
+++ stores/user.ts
@@ -19,18 +19,94 @@ import { ref } from 'vue';
 import { useFirestore, useFirebaseAuth, useCollection } from 'vuefire'; 
 import { collection, doc, setDoc, getDoc, writeBatch } from 'firebase/firestore';
 
-export const useUserStore = defineStore('user', () => {
-    // Reactive references
-    
+const db = useFirestore();
+const auth = useFirebaseAuth()!;
+// const route = useRoute();
+const router = useRouter();
+
+// const user = ref(useCurrentUser()); // holds the current user state
+
+export const useUserStore = defineStore('user', {
+    //using option stores
+
+    /*
     const db = useFirestore();
     const auth = useFirebaseAuth()!;
-    const user = useCurrentUser(); // holds the current user state
-
-    // const usersRef = useCollection(collection(db, 'users'));
-
     // const route = useRoute();
-    const router = useRouter();
+    const router = useRouter();*/
 
+    state: () => {
+        return {
+            user: useCurrentUser(), // holds the current user state
+            displayName: '',
+            username: '',
+            email: '',
+            photoURL: '',
+            emailVerified: false,
+        };
+    },
+    getters: {
+        //getters:
+        getUser(state) {
+            return state.user;
+        },
+        getDisplayName(state) {
+            return state.displayName;
+        },
+        getUsername(state) {
+            return state.username;
+        },
+        getEmail(state) {
+            return state.email;
+        },
+        getPhotoURL(state) {
+            return state.photoURL;
+        },
+        getEmailVerified(state) {
+            return state.emailVerified;
+        },
+    },
+    actions: {
+        //sign in:
+        async signIn(email: any, password: any) {
+            if(this.emailVerified){
+                try {
+                    signInWithEmailAndPassword(auth, email.value, password.value);
+                    router.push('/'); // Redirect the user to a confirmed page
+                } catch (error) {
+                    console.error('Login failed:', error);
+                    // let the form component display the error
+                    return error;
+                }
+            }
+            else{
+                console.log('Email not verified');
+                return 'Email not verified';
+            }
+            this.user = useCurrentUser().value;
+        },
+
+        //sign out:
+        async signOut() {
+            await auth.signOut();
+            this.user = null;
+        },
+    
+    },
+
+    /*
+
+    // Reactive references
+    const user = ref(useCurrentUser()); // holds the current user state
+    // const usersRef = ref(useCollection(collection(db, 'users')));
+
+    
+
+    //getters:
+
+
+
+    //actions:
 
     // Auth methods
     const signIn = async (email: any, password: any) => {
@@ -73,6 +149,6 @@ export const useUserStore = defineStore('user', () => {
         signIn, // method to sign in
         signOut, // method to sign out
         getData,
-    }
+    }*/
     
 });
