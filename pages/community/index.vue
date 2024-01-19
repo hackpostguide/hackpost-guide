3794cc7350cb572ee3c62200ec06aa6127e2e4c8
Alex <53095140+CometBlazer@users.noreply.github.com>
2024-01-19 15:08:54 -0500

    updated community homepage

--- pages/community/index.vue
+++ pages/community/index.vue
@@ -1,14 +1,14 @@
 <template>
-    <!-- Todo: apply css grids for entire page, fix paddings/margins -->
+
     <div class="m-auto grid grid-cols-12">
 
         <div class="py-10 col-start-2 col-end-12">
-            <p class="Heading2 font-bold mb-4">Header Text</p>
-            <p class="bodyText">Lorem ipsum dolor sit amet consectetur adipisicing elit. Odit ea dolorum a saepe porro consectetur sit! Laudantium maxime ullam molestias.</p>
+            <p class="Heading2 font-bold mb-4">Coming soon!</p>
+            <p class="bodyText"></p>
         </div>
 
         <div class="col-start-2 col-end-12 grid grid-cols-1 sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8 mt-4">
-        <!-- <div class="grid grid-cols-1 sm:grid-cols-1 md:grid-cols-2 lg:grid-cols    -3 gap-8 mt-4"> -->
+        <!-- <div class="grid grid-cols-1 sm:grid-cols-1 md:grid-cols-2 lg:grid-cols    -3 gap-8 mt-4"> 
             <div class="col-span-1 ">
                 <ModuleCard to="/community/test" />
             </div> 
@@ -17,7 +17,7 @@
             </div> 
             <div class="col-span-1 ">
                 <ModuleCard to="/community/python"/>
-            </div> 
+            </div>  -->
 
         </div>
     </div>
