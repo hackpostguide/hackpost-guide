d1e2d3eaeed6203853d754848f02c4e11542d19f
Alex <53095140+CometBlazer@users.noreply.github.com>
2024-01-19 10:35:10 -0500

    Remove unnecessary media queries for mobile display

--- components/Navbar.vue
+++ components/Navbar.vue
@@ -90,16 +90,6 @@
     box-shadow: 0 3px 6px rgba(128, 128, 128, 0.2);
   }
 
-  @media (max-width: 768px) {
-    .mobileoff {
-      display: none;
-    }
-  }
-  @media (min-width: 768px) {
-    .mobileon {
-      display: none;
-    }
-  }
   </style>
   
   
