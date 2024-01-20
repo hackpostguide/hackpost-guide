4cbe2f75626a1a43ba1407af4fba542e4edc26ee
Alex <53095140+CometBlazer@users.noreply.github.com>
2024-01-20 11:34:21 -0500

    Add email submission logic to NewsLetter component

--- components/NewsLetter.vue
+++ components/NewsLetter.vue
@@ -19,24 +19,38 @@
 
 <script>
 import { ref } from 'vue'
+import { collection, addDoc, doc, setDoc, getDoc, writeBatch } from 'firebase/firestore';
+
+const db = useFirestore();
 
 export default {
   setup(props, context) {
     const form$ = ref(null)
 
     onMounted(() => {
-      form$.value.messageBag.clear()
+    if (form$.value) {
+        form$.value.messageBag.clear();
+    }
     })
 
-    const handleSubmit = () => {
+    const handleSubmit = async () => {
       form$.value.messageBag.clear()
       const email = form$.value.data.email;
       console.log(validateEmail(email));
       if (!validateEmail(email)) {
         form$.value.messageBag.append('Please enter a valid email address.')
       } else {
-        form$.value.messageBag.clear()
-        // Proceed with form submission logic (e.g., send data to Firestore)
+        try {
+            const docRef = await addDoc(collection(db, "newsletter"), {
+                email: email
+            });
+            console.log("Document written with ID: ", docRef.id);
+            form$.value.messageBag.append(email + ' has successfully been added to our mailing list!', 'message')
+        } catch (e) {
+            console.error("Error adding document: ", e);
+            form$.value.messageBag.append(e, 'error')
+        }
+        
       }
     }
 
