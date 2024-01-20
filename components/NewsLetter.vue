a6eb77da1bc5eb37fe4ebddce403cf1e796a69d6
Alex <53095140+CometBlazer@users.noreply.github.com>
2024-01-20 11:16:50 -0500

    Refactor form submission logic in NewsLetter component

--- components/NewsLetter.vue
+++ components/NewsLetter.vue
@@ -1,50 +1,54 @@
 <template>
     <div>
-        <Vueform ref="form$" validate-on="step">
+        <Vueform ref="form$" @submit="handleSubmit">
             <TextElement
               name="email"
               input-type="email"
-              :rules="[
-                'nullable',
-                'email',
-              ]"
-              :columns="{
-                container: 9,
-              }"
+              :columns="{ container: 9 }"
               placeholder="Your email"
-
-              :messages="{
-                email: 'Please fill in a valid email address'
-              }"
             />
             <ButtonElement
               name="submit"
               button-label="Submit"
               :submits="true"
-              :columns="{
-                container: 3,
-              }"
+              :columns="{ container: 3 }"
             />
-          </Vueform>
-
+        </Vueform>
     </div>
 </template>
 
 <script>
-import { ref, onMounted } from 'vue'
+import { ref } from 'vue'
 
 export default {
   setup(props, context) {
     const form$ = ref(null)
 
+    onMounted(() => {
+      form$.value.messageBag.clear()
+    })
+
+    const handleSubmit = () => {
+      form$.value.messageBag.clear()
+      const email = form$.value.data.email;
+      console.log(validateEmail(email));
+      if (!validateEmail(email)) {
+        form$.value.messageBag.append('Please enter a valid email address.')
+      } else {
+        form$.value.messageBag.clear()
+        // Proceed with form submission logic (e.g., send data to Firestore)
+      }
+    }
+
+    function validateEmail(email) {
+        const re = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,6}$/;
+        return re.test(String(email).toLowerCase());
+    }
+
     return {
-      form$
+      form$,
+      handleSubmit
     }
   }
 }
-    
 </script>
-
-<style scoped>
-
-</style>
