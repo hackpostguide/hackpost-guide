b089409d27e627d87402a8ded31fbdc38d55d047
Alex <53095140+CometBlazer@users.noreply.github.com>
2024-01-22 20:46:35 -0500

    Updated signup page layout

--- pages/signup.vue
+++ pages/signup.vue
@@ -1,11 +1,11 @@
 <template>
     <div class="grid grid-cols-12">
-        <p>
-            
-        </p>
-        <div class="col-start-2 col-end-12">
-            <div>
-                <UserCreateAccount />
+        <div class="col-start-2 col-end-12 py-10">
+            <h1 class="Heading2 flex justify-center">
+                Create Account
+            </h1>
+            <div class="flex justify-center items-center h-screen">
+                <UserCreateAccount class="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4" />
             </div>
         </div>
     </div>
