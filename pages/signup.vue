ebaa4afa81d46f5ee0452a57db35d14e30a8dda9
Alex <53095140+CometBlazer@users.noreply.github.com>
2024-01-22 20:52:01 -0500

    Update UserCreateAccount styling in signup page

--- pages/signup.vue
+++ pages/signup.vue
@@ -4,8 +4,8 @@
             <h1 class="Heading2 flex justify-center">
                 Create Account
             </h1>
-            <div class="flex justify-center items-center h-screen">
-                <UserCreateAccount class="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4" />
+            <div class="flex justify-center pt-6">
+                <UserCreateAccount class="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4 overflow-y-auto max-h-[900px]" />
             </div>
         </div>
     </div>
